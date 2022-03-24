3c756a07e839dd64394b2b35983564e66d512da8
Advaith U <45172876+advaith-unnikrishnan@users.noreply.github.com>
2022-03-24 18:58:44 +0530

    Fix: Display Location

--- assets/scripts/dashboard.js
+++ assets/scripts/dashboard.js
@@ -2,37 +2,61 @@ const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
 const themeToggler = document.querySelector(".theme-toggler");
-
+let count = 0;
 
 // Show sidebar
-menuBtn.addEventListener('click' , () => {
-    sideMenu.style.display = 'block';
-    closeBtn.style.display = 'block';
-})
+menuBtn.addEventListener("click", () => {
+  sideMenu.style.display = "block";
+  closeBtn.style.display = "block";
+});
 
 // close sidebar
-closeBtn.addEventListener('click', () => {
-    sideMenu.style.display = 'none';
-})
+closeBtn.addEventListener("click", () => {
+  sideMenu.style.display = "none";
+});
 
 // change theme
-themeToggler.addEventListener('click', () => {
-    document.body.classList.toggle('dark-theme-variables');
+themeToggler.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme-variables");
 
-    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
-    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
-})
+  themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
+  themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
+});
 
 // Fill locations in table
 
-Locations.forEach(location => {
-    const tr = document.createElement('tr')
-    const trContent = `
-                    <td>${location.lat}</td>
-                    <td>${location.lon}</td>
-                    <td>${location.precipitation}</td>
-                    <td class="${location.status === 'Risky' ? 'danger' : location.status === 'Safe' ? 'success' : none }">${location.status}</td>`;
-    tr.innerHTML = trContent;
-    document.querySelector('table tbody').appendChild(tr);
-})
+for (let i = 0; i < 6; i++) {
+  const tr = document.createElement("tr");
+  const trContent = `
+                    <td>${Locations[i].lat}</td>
+                    <td>${Locations[i].lon}</td>
+                    <td>${Locations[i].precipitation}</td>
+                    <td class="${
+                      Locations[i].status === "Risky"
+                        ? "danger"
+                        : Locations[i].status === "Safe"
+                        ? "success"
+                        : none
+                    }">${Locations[i].status}</td>`;
+  tr.innerHTML = trContent;
+  document.querySelector("table tbody").appendChild(tr);
+}
 
+function showAll() {
+  Locations.forEach((location) => {
+    const tr = document.createElement("tr");
+    const trContent = `
+                        <td>${location.lat}</td>
+                        <td>${location.lon}</td>
+                        <td>${location.precipitation}</td>
+                        <td class="${
+                          location.status === "Risky"
+                            ? "danger"
+                            : location.status === "Safe"
+                            ? "success"
+                            : none
+                        }">${location.status}</td>`;
+    tr.innerHTML = trContent;
+    document.querySelector("table tbody").appendChild(tr);
+  });
+}
